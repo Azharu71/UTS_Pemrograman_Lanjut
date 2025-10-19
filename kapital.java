e1449cf783bbf40e586aee754249fcf50cdc6c2f
Azharu2 <azharfrendly@gmail.com>
2025-10-19 10:36:43 +0700

    Fix 1

--- kapital.java
+++ kapital.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class subrutin {
+public class kapital {
 
     // Subrutin/fungsi untuk mencetak versi huruf kapital dari string
     public static void printCapitalized(String str) {
